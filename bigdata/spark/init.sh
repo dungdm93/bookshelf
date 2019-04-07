f531615ad6d09d5cf82a7beace0f9c1de882f3be
Đặng Minh Dũng <dungdm93@live.com>
2019-04-07 18:17:13 +0700

    provision with kubectl

--- bigdata/spark/init.sh
+++ bigdata/spark/init.sh
@@ -5,3 +5,9 @@ export SPARK_VERSION=2.4.1
 export SPARK_HOME=/opt/spark
 export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.10.7-src.zip"
 export PATH=${SPARK_HOME}/bin:${PATH}
+
+alias ..='cd ..'
+
+if command -v kubectl >/dev/null 2>&1; then
+    source <(kubectl completion bash)
+fi
