4f6c700e33e1a226cfd4890a3b8fad6fb8d65a20
Đặng Minh Dũng <dungdm93@live.com>
2019-04-07 17:58:10 +0700

    spark Vagrantfile

---
+++ bigdata/spark/init.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:/bin/java::")
+export SPARK_VERSION=2.4.1
+export SPARK_HOME=/opt/spark
+export PYTHONPATH="${SPARK_HOME}/python:${SPARK_HOME}/python/lib/py4j-0.10.7-src.zip"
+export PATH=${SPARK_HOME}/bin:${PATH}
