1dc6f49a87c0f35ff079fdb89e011c46108d0f74
Đặng Minh Dũng <dungdm93@live.com>
2020-01-01 17:27:24 +0700

    presto add mysql & postgres db

---
+++ bigdata/presto/database/mysql/1-sakila-schema.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20121424a3bf22a68b3de0129eda1d84e5be6083c3e26e9e089f211a63aee68a
+size 24278
---
+++ bigdata/presto/database/mysql/2-sakila-data.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:167e865818c04ef6f17562413adbb563a9c1b97b384720187deb9c5f00d6445a
+size 3351702
