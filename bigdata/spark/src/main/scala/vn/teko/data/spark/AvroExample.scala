260afa11fe0ec7414d2562f2763fd8d7a53261e0
Đặng Minh Dũng <dungdm93@live.com>
2020-02-22 19:59:50 +0700

    update spark example

--- bigdata/spark/src/main/scala/vn/teko/data/spark/AvroExample.scala
+++ bigdata/spark/src/main/scala/vn/teko/data/spark/AvroExample.scala
@@ -1,9 +1,24 @@
 package vn.teko.data.spark
 
-//import org.apache.spark.
+import java.io.File
+
+import org.apache.avro.Schema
+import org.apache.spark.sql.SparkSession
 
 object AvroExample extends App {
-//    val spark  = SparkSession.builder().master("local[1]")
-//        .appName("SparkByExamples.com")
-//        .getOrCreate()
+    val spark = SparkSession.builder()
+        .master("local[1]")
+        .appName("SparkExamples")
+        .getOrCreate()
+
+    val schema = new Schema.Parser().parse(new File("src/main/resources/mysql.avsc"))
+    val df = spark.read
+        .format("avro")
+        .option("avroSchema", schema.toString)
+        .load("src/main/resources/mysql.avro")
+
+    df.show()
+    df.printSchema()
+
+    spark.close()
 }
